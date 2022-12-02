06e55418712a5bdc2118c84945ff5b28c4a24166
JhonataLacerda <jonata_5000@hotmail.com>
2022-12-02 14:05:57 -0300

    Subindo projeto spring boot

---
+++ blog/src/test/java/com/br/blog/wineron/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.blog.wineron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
